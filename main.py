95df8fcae7da210f9c7da2bd1ce943e6e8ca1328
Raju8115 <117435076+Raju8115@users.noreply.github.com>
2026-02-04 17:22:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -123,5 +123,5 @@ def shutdown_event():
 
 # Run app directly with uvicorn
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORTT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
